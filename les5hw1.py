34840519bbf7dbe1a13861c0ed34268413f5e0f6
Thomas den Otter <thomasdenotter6@hotmail.com>
2023-07-22 13:32:48 +0200

    opdrachtklaar

---
+++ les5hw1.py
@@ -0,0 +1,15 @@
+logischenaam1 = 10
+logischenaam2 = 0.5
+totaal = logischenaam1 * logischenaam2
+print(totaal)
+
+#Opdracht 2#
+for i in range(10, 20, 1):
+ print(f"{i} maal twee is {i*2}")
+
+#opdracht3
+c = 30
+d = "Deborah"
+print(f"Haar naam is {d},ze is {c} jaar oud")
+
+
